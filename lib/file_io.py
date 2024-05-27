a100e49d1716b3ba2c8e746e3fb86f411ac19652
victor@victor <victor.dmaina@gmail.com>
2024-05-27 17:53:07 +0300

    complete tests

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
 def write_file(file_name, file_content):
     pass
 
+def write_file(file_name, file_content):
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'w') as file:
+        file.write(file_content)    
+        
 def append_file(file_name, append_content):
     pass
 
+def append_file(file_name, append_content):
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'a') as file:
+        file.write(append_content)
+
 def read_file(file_name):
     pass
+
+def read_file(file_name):
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'r') as file:
+        return file.read()
